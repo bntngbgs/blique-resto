4097135dad02b463eeabe4df9c51e86fc214156c
Bintang Bagus Pangestu <bintangbgs16@gmail.com>
2023-09-10 08:47:30 +0700

    add form and footer section

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const navButton = document.getElementById('menu-button');
 const navItems = document.querySelector('.navbar');
+const input = document.querySelector('#phone');
+
 navButton.addEventListener('click', () => {
   navItems.classList.toggle('hidden');
   navItems.classList.toggle('flex');
@@ -9,4 +11,11 @@ navButton.addEventListener('click', () => {
   } else {
     navItems.classList.remove('justify-center');
   }
+});
+
+window.intlTelInput(input, {
+  initialCountry: 'id',
+  separateDialCode: true,
+  utilsScript:
+    'https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js',
 });
