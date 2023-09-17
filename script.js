7f0d55643e44165053209e8ab918119a2aba218b
Bintang Bagus Pangestu <bintangbgs16@gmail.com>
2023-09-17 09:27:29 +0700

    add active navbar with scroll events

--- script.js
+++ script.js
@@ -5,7 +5,21 @@ const sections = document.querySelectorAll('section[id]');
 const navLinks = document.querySelectorAll('.navbar a');
 const reservationForm = document.getElementById('reservation');
 const reservationModal = document.querySelector('.modal');
+const errorMsg = document.querySelector('#error-msg');
+const validMsg = document.querySelector('#valid-msg');
+const reservationModalContainer = document.getElementById('reservation-modal');
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const errorMap = [
+  'Invalid number',
+  'Invalid country code',
+  'Phone number is too short',
+  'Phone number is too long',
+  'Invalid number',
+];
+
+if (window.innerWidth < 641) {
+  navLinks[0].classList.remove('active');
+}
 
 // EVENT : toggle mobile navigation
 navButton.addEventListener('click', () => {
@@ -19,8 +33,29 @@ navButton.addEventListener('click', () => {
   }
 });
 
+// EVENT :
+navLinks.forEach((item) => {
+  item.addEventListener('click', () => {
+    navItems.classList.add('hidden');
+    navItems.classList.remove('flex');
+  });
+});
+
+// EVENT : add shadow to header when scroll
+window.addEventListener('scroll', () => {
+  let scroll = window.scrollY;
+  if (scroll > 0) {
+    document.getElementById('header').classList.add('shadow');
+  } else {
+    document.getElementById('header').classList.remove('shadow');
+  }
+});
+
 // EVENT : active navbar when scroll
 window.addEventListener('scroll', () => {
+  if (window.innerWidth < 641) {
+    return;
+  }
   sections.forEach((item) => {
     let top = window.scrollY;
     let offset = item.offsetTop - 100;
@@ -50,19 +85,73 @@ reservationForm.addEventListener('submit', (e) => {
   const fullName = document.querySelector('input[type="text"]');
   const email = document.querySelector('input[type="email"]');
   const notes = document.querySelector('textarea');
+  const id = Math.floor(100000 + Math.random() * 900000);
+  let phoneNumber;
 
-  reservationModal.innerHTML = `<p>full name : ${fullName.value}</p>
-  <p>email: ${email.value}</p>
-  <p>notes: ${notes.value}</p>`;
+  reset();
+  if (input.value.trim()) {
+    if (iti.isValidNumber()) {
+      phoneNumber = input.value;
+    } else {
+      input.classList.add('error');
+      const errorCode = iti.getValidationError();
+      errorMsg.innerHTML = errorMap[errorCode];
+      errorMsg.classList.remove('hidden');
+      errorMsg.style.color = '#ef4444';
+      return;
+    }
+  }
+
+  if (!notes.value) {
+    notes.value = '-';
+  }
+
+  renderModal(id, fullName.value, email.value, phoneNumber, notes.value);
+  reservationModalContainer.classList.remove('hidden');
+  reservationModalContainer.classList.add(
+    'flex',
+    'justify-center',
+    'items-center'
+  );
+  reservationForm.reset();
+});
+
+// EVENT : close reservation modal
+reservationModal.addEventListener('click', (e) => {
+  console.log(e.target);
+  if (e.target.classList.contains('close-modal')) {
+    reservationModalContainer.classList.remove('flex');
+    reservationModalContainer.classList.add('hidden');
+  }
 });
 
 // PLUGINS : intl-tel-input form
-window.intlTelInput(input, {
+const iti = window.intlTelInput(input, {
   initialCountry: 'id',
   separateDialCode: true,
   utilsScript:
     'https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js',
 });
+
+const reset = () => {
+  input.classList.remove('error');
+  errorMsg.innerHTML = '';
+  errorMsg.classList.add('hidden');
+  validMsg.classList.add('hidden');
+};
+
+input.addEventListener('change', reset);
+input.addEventListener('keyup', reset);
+
+// FUNCTION : render successful modal reservation form
+function renderModal(id, name, email, phone, notes) {
+  const spanModal = document.querySelectorAll('.modal span');
+  spanModal[0].innerText = id;
+  spanModal[1].innerText = name;
+  spanModal[2].innerText = email;
+  spanModal[3].innerText = phone;
+  spanModal[4].innerText = notes;
+}
 
 // LEGACY CODE
 
